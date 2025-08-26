dc6961174c18fa3a58dfb1b35cedf30dbb8e0c11
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-08-26 09:53:25 +0000

    save

--- posts/index.js
+++ posts/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const {randomBytes} = reuire('crypto')
+const {randomBytes} = require('crypto')
+const bodyParser = require('body-parser')
 
 const app = express();
 
 const posts = {};
 
+app.use(bodyParser.json())
+
 app.get('/posts', (req, res)=>{
-    res.send(posts)
+    res.send(posts);
 });
 
 app.post('/posts', (req, res)=>{
-    const id = randomBytes(4).toString('hex')
+    const id = randomBytes(4).toString('hex');
+    const {title} = req.body;
+    posts[id] = {id, title};
 }); 
 
 app.listen(4000, ()=>{
